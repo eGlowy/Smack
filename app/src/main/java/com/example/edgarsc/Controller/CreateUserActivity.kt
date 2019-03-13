57f88a043213f464fe01c12ed78361eb9f3f5ee9
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-13 11:15:11 +0200

    Create user layout lesson 10 Created authservice registerUser API call and hooked up with the view;

--- app/src/main/java/com/example/edgarsc/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/edgarsc/Controller/CreateUserActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.edgarsc.R
+import com.example.edgarsc.Services.AuthService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -49,6 +50,8 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View){
+        AuthService.registerUser(this, "j@j.lv", "123456") {
 
+        }
     }
 }
