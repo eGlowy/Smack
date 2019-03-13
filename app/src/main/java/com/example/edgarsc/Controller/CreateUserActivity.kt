63e29dc3b85c19708b0b1c85aa7b3eb765590097
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-13 14:51:25 +0200

    Create user layout lesson 13 Spinner when API call is in progress; Validation of fields so blank information is not sent; BroadCast constanst for other views;

--- app/src/main/java/com/example/edgarsc/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/edgarsc/Controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package com.example.edgarsc.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import com.example.edgarsc.R
 import com.example.edgarsc.Services.AuthService
 import com.example.edgarsc.Services.UserDataService
+import com.example.edgarsc.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +22,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View){
@@ -51,29 +56,56 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View){
+        enableSpinner(true)
         val userName = createUserUsernameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
 
-
-        AuthService.registerUser(this, email, password) {registerSuccess ->
-            if (registerSuccess){
-                AuthService.loginUser(this, email,password) {
-                    loginSuccess -> if (loginSuccess){
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.registerUser(this, email, password) {registerSuccess ->
+                if (registerSuccess){
+                    AuthService.loginUser(this, email,password) {
+                            loginSuccess -> if (loginSuccess){
                         println(AuthService.authToken)
                         println(AuthService.userEmail)
                         AuthService.createUser(this,userName,email,userAvatar , avatarColor) {createSuccess ->
                             if (createSuccess){
-                                println(UserDataService.avatarName)
-                                println(UserDataService.avatarColor)
-                                println(UserDataService.name)
+                                //Izsūta informāciju no aktivitātes , lai citas aktivitātes kuras klausās šo broadcast saņemtu informāciju par datu maiņu un citām lietām.
+                                val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                enableSpinner(false)
                                 finish()
+                            } else {
+                                errorToast()
                             }
                         }
+                    } else {
+                        errorToast()
                     }
+                    }
+                } else {
+                    errorToast()
                 }
             }
-
+        } else {
+            Toast.makeText(this,"Make sure user name, email and password are filled in.", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
         }
+    }
+
+    fun errorToast(){
+        Toast.makeText(this,"Something went wrong, please try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean){
+        if (enable){
+            createSpinner.visibility = View.VISIBLE
+        } else {
+            createSpinner.visibility = View.INVISIBLE
+        }
+        createUserBtn.isEnabled = !enable
+        createUserAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
     }
 }
