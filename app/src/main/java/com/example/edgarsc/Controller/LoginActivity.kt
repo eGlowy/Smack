fc6df92e4e068810f38803ab0d6812372afb891e
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-13 17:34:19 +0200

    Create user layout lesson 15 login funcionality

--- app/src/main/java/com/example/edgarsc/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/edgarsc/Controller/LoginActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.edgarsc.R
+import com.example.edgarsc.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -20,6 +22,17 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View){
-
+        val userEmail = loginEmailTxt.text.toString()
+        val userPassword = loginPasswordTxt.text.toString()
+        AuthService.loginUser(this, userEmail, userPassword) {loginSuccess ->
+            if (loginSuccess){
+                println(AuthService.authToken)
+                println(AuthService.userEmail)
+                AuthService.findUserByEmail(this) { findSuccess ->
+                    if (findSuccess) {}
+                    finish()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/edgarsc/Services/AuthService.kt
+++ app/src/main/java/com/example/edgarsc/Services/AuthService.kt
@@ -1,14 +1,14 @@
 package com.example.edgarsc.Services
 
 import android.content.Context
+import android.content.Intent
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.edgarsc.Utilities.URL_CREATE_USER
-import com.example.edgarsc.Utilities.URL_LOGIN
-import com.example.edgarsc.Utilities.URL_REGISTER
+import com.example.edgarsc.Utilities.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.util.*
@@ -68,7 +68,7 @@ object AuthService {
 
         }, Response.ErrorListener {
             //This ir where we deal with out error
-                error -> Log.d("ERROR", "Could not register user: $error")
+                error -> Log.d("ERROR", "Could not login user: $error")
             complete(false)
         }) {
             override fun getBodyContentType(): String {
@@ -124,5 +124,37 @@ object AuthService {
         }
 
         Volley.newRequestQueue(context).add(createUserRequest)
+    }
+
+    fun findUserByEmail(context: Context, complete: (Boolean) -> Unit){
+        val findUserRequest = object : JsonObjectRequest(Method.GET, "$URL_GET_USER$userEmail", null, Response.Listener {
+            response ->
+            try {
+                UserDataService.name = response.getString("name")
+                UserDataService.email = response.getString("email")
+                UserDataService.avatarColor = response.getString("avatarColor")
+                UserDataService.avatarName = response.getString("avatarName")
+                UserDataService.id = response.getString("_id")
+
+                val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                LocalBroadcastManager.getInstance(context).sendBroadcast(userDataChange)
+                complete(true)
+            } catch (e:JSONException) {
+                Log.d("JSON", "EXC:" + e.localizedMessage)
+                complete(false)
+            }
+
+        }, Response.ErrorListener {error -> Log.d("ERROR", "Could not find user: $error")
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer $authToken")
+                return  headers
+            }
+        }
+        Volley.newRequestQueue(context).add(findUserRequest)
     }
 }
