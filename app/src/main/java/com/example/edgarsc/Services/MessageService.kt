33bb0b07f88081b49bad90aded5e881f47b7b498
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-26 11:07:28 +0200

    Created Get messages by channel id Created Constant for message retrieve endpoint After logout clear message and channels

--- app/src/main/java/com/example/edgarsc/Services/MessageService.kt
+++ app/src/main/java/com/example/edgarsc/Services/MessageService.kt
@@ -1,14 +1,13 @@
 package com.example.edgarsc.Services
 
-import android.content.Context
 import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.Volley
 import com.example.edgarsc.Controller.App
 import com.example.edgarsc.Model.Channel
 import com.example.edgarsc.Model.Message
 import com.example.edgarsc.Utilities.URL_GET_CHANNELS
+import com.example.edgarsc.Utilities.URL_GET_MESSAGES
 import org.json.JSONException
 
 object MessageService {
@@ -52,5 +51,56 @@ object MessageService {
                 }
             }
         App.prefs.requestQueue.add(channelsRequest)
+    }
+
+    fun getMessages(channelId: String, complete: (Boolean) -> Unit) {
+        val url = "$URL_GET_MESSAGES$channelId"
+        val messageRequest =
+                object : JsonArrayRequest(Method.GET, url, null, Response.Listener { response ->
+                    clearMessages()
+                    try {
+
+                        for (x in 0 until response.length()) {
+                            val message = response.getJSONObject(x)
+
+                            val id = message.getString("_id")
+                            val msgBody = message.getString("messageBody")
+                            val channelId = message.getString("channelId")
+                            val userName = message.getString("userName")
+                            val userAvatar = message.getString("userAvatar")
+                            val userAvatarColor = message.getString("userAvatarColor")
+                            val timeStamp = message.getString("timeStamp")
+
+                            val newMessage = Message(msgBody, userName, channelId, userAvatar,userAvatarColor,id,timeStamp)
+                            this.messages.add(newMessage)
+                        }
+                        complete(true)
+
+                    } catch (e: JSONException) {
+                        Log.d("JSON", "EXC:" + e.localizedMessage)
+                    }
+                }, Response.ErrorListener {
+                    Log.d("ERROR", "Could not retrieve messages")
+                    complete(false)
+                }) {
+                    override fun getBodyContentType(): String {
+                        return "application/json; charset=utf-8"
+                    }
+
+                    override fun getHeaders(): MutableMap<String, String> {
+                        val headers = HashMap<String, String>()
+                        headers.put("Authorization", "Bearer ${App.prefs.authToken}")
+                        return headers
+                    }
+                }
+        App.prefs.requestQueue.add(messageRequest)
+    }
+
+    fun clearMessages(){
+        messages.clear()
+    }
+
+    fun clearChannels(){
+        channels.clear()
     }
 }
